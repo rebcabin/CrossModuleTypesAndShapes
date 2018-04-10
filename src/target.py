206250a15844e96ae31a4cd1ec82684edea573e2
rebcabin <bc.beckman@gmail.com>
2018-04-10 10:03:29 -0700

    add cross-module shape checking

--- src/target.py
+++ src/target.py
@@ -1,8 +1,15 @@
+import numpy as np
+
 
 class ArbitraryType(object):
     def __init__(self):
         self.arbitrary_attribute = 42
         pass
 
+
 def is_arbitrary_type(some_type):
     return some_type is ArbitraryType
+
+
+def is_rank_one_tensor(some_object):
+    return len(np.shape(some_object)) == 1
--- tests/test_type_transfer.py
+++ tests/test_type_transfer.py
@@ -1,5 +1,8 @@
+import numpy as np
+
 from src.target import ArbitraryType
 from src.target import is_arbitrary_type
+from src.target import is_rank_one_tensor
 
 
 def test_type_locally():
@@ -10,3 +13,8 @@ def test_type_locally():
 def test_type_other_module():
     a = ArbitraryType()
     assert is_arbitrary_type(type(a))
+
+
+def test_shape_other_module():
+    na = np.array([ArbitraryType(), ArbitraryType()])
+    assert is_rank_one_tensor(na)
